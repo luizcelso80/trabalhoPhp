d85e675c2fcef3ab506f6aa8acd65a2c9b1630fa
ZataraWin <master@limabauru.com.br>
2017-05-09 06:37:14 -0900

    Declaracao finalizada

--- pendencia.php
+++ pendencia.php
@@ -47,7 +47,7 @@ if(isset($_GET['alu_pendencia'])){
             });
             $("#content>.btn-danger").click(function(e){
                 //e.preventDefault();
-                alert("deletou");
+                //alert("deletou");
             });
         });
 
@@ -60,6 +60,9 @@ if(isset($_GET['alu_pendencia'])){
     <div class="container-fluid" id="content">
         <?php require_once('formPesquisa.php') ?>
         <?php require_once('listar.php'); ?>
+        
+            <a type="button" href="imprimir.php?alu_pendencia=<?php echo isset($_GET['alu_pendencia'])? $_GET['alu_pendencia']:NULL ; ?>" target="_blank" class="fa fa-file-pdf-o btn btn-danger" aria-hidden="true"></a>
+            <a type="button" href="excel.php?alu_pendencia=<?php echo isset($_GET['alu_pendencia'])? $_GET['alu_pendencia']:NULL ; ?>" class="fa fa-file-excel-o btn btn-success" aria-hidden="true"></a>
         
     </div>
 
